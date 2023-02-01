5179705805cbf85164da1b7413ac3967ada37eef
AleksaYO <113035091+AleksaYO@users.noreply.github.com>
2023-02-01 09:25:28 +0000

    Deploying to gh-pages from @ AleksaYO/goit-js-hw-09@b3ae7c7321c1466f200b3b99f0ae18fc250720c3 🚀

---
+++ 01-color-switcher.ae0ab9e4.js
@@ -0,0 +1,2 @@
+!function(){var e=document.querySelector("[data-start]"),t=document.querySelector("[data-stop]"),a=document.querySelector("body");e.addEventListener("click",(function(){e.disabled=!0,t.disabled=!1,n=setInterval((function(){a.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}),1e3)})),t.addEventListener("click",(function(){e.disabled=!1,t.disabled=!0,clearInterval(n)}));var n=1}();
+//# sourceMappingURL=01-color-switcher.ae0ab9e4.js.map
